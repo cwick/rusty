fc89555bd9538bea6f6ed6b2acdf170c9728080e
Carmen Wick <CarmenWick@gmail.com>
2021-07-17 19:56:55 -0700

    Hello window

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,15 +1,15 @@
 fn main() {
-  windows::build! {
-      Windows::Win32::Foundation::{HINSTANCE, PSTR, RECT},
-      Windows::Win32::System::LibraryLoader::GetModuleHandleA,
-      Windows::Win32::System::WindowsProgramming::INFINITE,
-      Windows::Win32::UI::WindowsAndMessaging::{
-          MessageBoxA,
-          AdjustWindowRect, CreateWindowExA, DefWindowProcA, DispatchMessageA, GetWindowLongA,
-          GetWindowLongPtrA, LoadCursorW, PeekMessageA, PostQuitMessage, RegisterClassExA,
-          SetWindowLongA, SetWindowLongPtrA, ShowWindow, TranslateMessage, CREATESTRUCTA,
-          CW_USEDEFAULT, IDC_ARROW, MSG, WM_CREATE, WM_DESTROY, WM_KEYDOWN, WM_KEYUP, WM_PAINT,
-          WM_QUIT, WNDCLASSEXA, WNDCLASS_STYLES,
-      },
-  };
+    windows::build! {
+        Windows::Win32::Foundation::{HINSTANCE, PSTR, RECT, HWND, WPARAM, LPARAM, LRESULT},
+        Windows::Win32::System::LibraryLoader::GetModuleHandleA,
+        Windows::Win32::System::WindowsProgramming::INFINITE,
+        Windows::Win32::UI::WindowsAndMessaging::{
+            MessageBoxA,
+            AdjustWindowRect, CreateWindowExA, DefWindowProcA, DispatchMessageA, GetWindowLongA,
+            GetWindowLongPtrA, LoadCursorW, PeekMessageA, PostQuitMessage, RegisterClassExA,
+            SetWindowLongA, SetWindowLongPtrA, ShowWindow, TranslateMessage, CREATESTRUCTA,
+            CW_USEDEFAULT, IDC_ARROW, MSG, WM_CREATE, WM_DESTROY, WM_KEYDOWN, WM_KEYUP, WM_PAINT,
+            WM_QUIT, WNDCLASSEXA, WNDCLASS_STYLES,
+        },
+    };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,64 @@
+use bindings::Windows::Win32::Foundation::*;
+use bindings::Windows::Win32::System::LibraryLoader::*;
 use bindings::Windows::Win32::UI::WindowsAndMessaging::*;
 
+extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    match message {
+        WM_DESTROY => {
+            unsafe { PostQuitMessage(0) };
+            LRESULT::default()
+        }
+        _ => unsafe { DefWindowProcA(window, message, wparam, lparam) },
+    }
+}
+
 fn main() -> windows::Result<()> {
+    let instance = unsafe { GetModuleHandleA(None) };
+
+    let wc = WNDCLASSEXA {
+        cbSize: std::mem::size_of::<WNDCLASSEXA>() as u32,
+        style: CS_HREDRAW | CS_VREDRAW | CS_OWNDC,
+        lpfnWndProc: Some(wndproc),
+        hInstance: instance,
+        hCursor: unsafe { LoadCursorW(None, IDC_ARROW) },
+        lpszClassName: PSTR(b"RustWindowClass\0".as_ptr() as _),
+        ..Default::default()
+    };
+
     unsafe {
-        MessageBoxA(None, "Text", "Caption", MB_OK);
+        RegisterClassExA(&wc);
+
+        let hwnd = CreateWindowExA(
+            Default::default(),
+            "RustWindowClass",
+            "Hello Windows",
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            800,
+            600,
+            None, // no parent window
+            None, // no menus
+            instance,
+            &mut 0 as *mut _ as _,
+        );
+
+        ShowWindow(hwnd, SW_SHOW)
+    };
+
+    loop {
+        let mut message = MSG::default();
+
+        if unsafe { PeekMessageA(&mut message, None, 0, 0, PM_REMOVE) }.into() {
+            unsafe {
+                TranslateMessage(&message);
+                DispatchMessageA(&message);
+            }
+
+            if message.message == WM_QUIT {
+                break;
+            }
+        }
     }
 
     Ok(())
